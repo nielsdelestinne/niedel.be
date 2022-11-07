28346ad624db13b953a21eb77648db936364b24f
nielsdelestinne <nielsdelestinne@gmail.com>
2022-11-07 20:38:47 +0100

    Proof reading & automatic download functionality added

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,10 +1,28 @@
 (function($){
+
+  $('.downloading-cv').hide();
+
   $(function(){
 
     $('.scrollspy').scrollSpy({
       scrollOffset: 0
     });
     $('.sidenav').sidenav();
+
+    $('.download-cv').on("click", () => {
+
+      $('.downloading-cv').show();
+      $('.download-cv').hide();
+
+      html2canvas(document.body).then((canvas) => {
+        const containerElement = document.createElement("a");
+        containerElement.download = `niels_delestinne_cv_${Date.now()}.png`;
+        containerElement.href = canvas.toDataURL("application/pdf");
+        containerElement.click();
+        $('.download-cv').show();
+        $('.downloading-cv').hide();
+      });
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
